99cf2b9e7acf2564cc1876307fecfe74d0eea950
Paolo Petrocelli <ppetroce@gmail.com>
2017-03-30 00:39:34 -0300

    cambiado el texto de un lbl

--- DirectorioArchivos/Directorio.Designer.cs
+++ DirectorioArchivos/Directorio.Designer.cs
@@ -36,11 +36,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(171, 9);
+            this.label1.Location = new System.Drawing.Point(147, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 38);
+            this.label1.Size = new System.Drawing.Size(283, 38);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Ver Archivo";
+            this.label1.Text = "Agregar Archivos";
             // 
             // panelPrincipal
             // 
